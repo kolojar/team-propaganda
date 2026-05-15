b9b8e203765676fb719b168574dad082e46505d2
LukasJarolim <lukasjarolim01@seznam.cz>
2026-05-15 08:59:55 +0200

    Run script

---
+++ development.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Set this in config line: $servername = \"127.0.0.1:3307\";"
+echo "If connection fails, it can be because of running mariadb on this computer"
+cloudflared access tcp --hostname dbteampropaganda.kolojar.cz --url 127.0.0.1:3307 & php -S 127.0.0.1:5501
+
+
