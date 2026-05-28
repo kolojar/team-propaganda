304dbe9c465b5d0f93741bcff79718d81c6ef316
matkolo1 <matej.kor@email.cz>
2026-05-28 20:38:18 +0200

    update #24

--- admin/presets.php
+++ admin/presets.php
@@ -3,22 +3,23 @@ require "../assets/config.php";
 require "./adminFunctions.php";
 session_start();
 if (!isset($_SESSION["userId"])) {
-    //header("Location: loginAdmin.php");
+    header("Location: loginAdmin.php");
+    die;
 }
 if (isset($_POST["template"], $_POST["name"], $_POST["message"])) {
-    //echo "got ";
-    $template = $_POST["template"];
-    $name = $_POST["name"];
-    $message = $_POST["message"];
-    //echo "$template, $name, $message\n";
-    if (!isset($_POST["new"]) || $_POST["new"] == false) {
-        unlink("../templates/$template");
+    if ($_POST["message"] != null && $_POST["name"] != null) {
+        $template = $_POST["template"];
+        $name = $_POST["name"];
+        $message = $_POST["message"];
+        $file = fopen("../templates/$name", "w");
+        fwrite($file, $message);
+        fclose($file);
+        echo "succ";
+        exit;
+    } else {
+        echo "Neplatné použití funkce.";
+        die;
     }
-    $file = fopen("../templates/$name", "w");
-    fwrite($file, $message);
-    fclose($file);
-    echo "succ";
-    exit;
 }
 if (isset($_POST["file"])) {
     unlink("../templates/" . $_POST["file"]);
@@ -69,7 +70,6 @@ if (isset($_POST["file"])) {
         <div class="formButtonBoxHolder">
             <div class="formButtonBox">
                 <button id="save" class="purkynkaButton" form-icon="!save"></button>
-                <button id="savenew" class="purkynkaButton" form-icon="!saveAs"></button>
             </div>
         </div>
     </main>
@@ -77,9 +77,15 @@ if (isset($_POST["file"])) {
         console.log("html")
     </script>
     <script type="module">
-        import { SendPOSTDataToServerAsync } from "../formWebScripts/js/serverComunication.js";
-        import { SendToast } from "../formWebScripts/js/formScript.js";
-        import { FormDialogManager } from "../formWebScripts/js/formDialogScript.js";
+        import {
+            SendPOSTDataToServerAsync
+        } from "../formWebScripts/js/serverComunication.js";
+        import {
+            SendToast
+        } from "../formWebScripts/js/formScript.js";
+        import {
+            FormDialogManager
+        } from "../formWebScripts/js/formDialogScript.js";
         const dialogManager = new FormDialogManager()
         let selectedTemplate = "none";
 
@@ -97,11 +103,8 @@ if (isset($_POST["file"])) {
             })
         }
 
-        document.getElementById("save").addEventListener("click", (e) => {
-            save()
-        });
-        document.getElementById("savenew").addEventListener("click", (e) => {
-            save(true)
+        document.getElementById("save").addEventListener("click", async (e) => {
+            await save()
         });
 
         async function templateChange(template) {
@@ -118,7 +121,7 @@ if (isset($_POST["file"])) {
                 const progress = dialogManager.ShowProgress("Otevřít šablonu", "Probíhá načítání dat ze serveru, čekejte prosím...", true, true)
                 var request = new XMLHttpRequest();
                 request.open('GET', "../templates/" + template, true);
-                request.onload = async function () {
+                request.onload = async function() {
                     if (request.status >= 200 && request.status < 400) {
                         document.getElementById("message").setValue(request.responseText);
                     } else {
@@ -127,7 +130,7 @@ if (isset($_POST["file"])) {
                         await dialogManager.OpenAlert("Otevřít šablonu", "Informace nemohly být načteny, opakujte akci později.<br>Důvod: " + request.responseText, true, true)
                     }
                 };
-                request.onerror = async function () {
+                request.onerror = async function() {
                     SendToast("Nelze otevřít šablonu!", "Nepodařilo se načíst informace.", "error")
                     progress.CloseDialog()
                     await dialogManager.OpenAlert("Otevřít šablonu", "Informace nemohly být načteny, opakujte akci později.<br>Důvod: Neznámá chyba.", true, true)
@@ -139,29 +142,41 @@ if (isset($_POST["file"])) {
             }
         }
 
-        async function save(nev = false) {
+        async function save() {
+            if (!await dialogManager.OpenConfirm("Uložit šablonu?", "Opradu si přejete uložit šablonu?<br>Předchozí obsah bude smazán.")) {
+                SendToast("Ukládání šablony zrušeno", "Ukládání šablony zrušeno.", "info")
+                return
+            }
             const data = new FormData();
-            data.append("new", nev)
             data.append("template", selectedTemplate)
             data.append("name", document.getElementById("name").getValue())
             data.append("message", document.getElementById("message").getValue())
 
             console.log(data);
-
+            const wait = dialogManager.ShowProgress("Odesílání dat na server", "Čekejte prosím", () => {}, 0)
             const [ok, res] = await SendPOSTDataToServerAsync("./presets.php", data);
+            wait.CloseDialog()
 
-            if (ok) SendToast("Odpověď serveru:", res, "ok")
-            else SendToast("Odpověď serveru", res, "error")
+            if (ok) {
+                SendToast("Odpověď serveru:", res, "ok")
+                window.location.reload()
+            } else SendToast("Odpověď serveru", res, "error")
         }
 
         async function delet(file) {
+            if (!await dialogManager.OpenConfirm("Smazat šablonu?", "Opravdu si přejete smazat tento soubor?<br>Tato akce nelze vzít zpět.")) {
+                SendToast("Mazání šablony zrušeno", "Mazání šablony zrušeno.", "info")
+                return
+            }
             const data = new FormData();
             data.append("file", file)
 
             const [ok, res] = await SendPOSTDataToServerAsync("./presets.php", data);
 
-            if (ok) SendToast("Odpověď serveru", res, "ok")
-            else SendToast("Odpověď serveru", res, "error")
+            if (ok) {
+                SendToast("Odpověď serveru", res, "ok")
+                location.reload()
+            } else SendToast("Odpověď serveru", res, "error")
         }
     </script>
 </body>
