17b8f299eac68b7287900203b21b3b1b18d246ad
LukasJarolim <lukasjarolim01@seznam.cz>
2026-06-04 19:31:39 +0200

    Removed old styles

--- user/main.php
+++ user/main.php
@@ -16,7 +16,6 @@ require '../assets/sharedFunctions.php';
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Uživatelský panel</title>
-    <link rel='stylesheet' href='../formWebScripts/css/sharedStyle.css'>
     <link rel='stylesheet' href='../formWebScripts/css/formStyle.css'>
     <link rel='stylesheet' href='../assets/style.css'>
     <link rel='stylesheet' href='./user.css'>
