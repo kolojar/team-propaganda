6355f7f9139e9777b66409810f66cb519f1fee4d
LukasJarolim <lukasjarolim01@seznam.cz>
2026-06-10 05:51:01 +0200

    Workign on company days

--- admin/companies.php
+++ admin/companies.php
@@ -33,6 +33,9 @@ require "./adminFunctions.php";
             }
             return "<a href='./sendMail.php?uid=$uid&isNILE=1'>$email</a>";
         }
+        function icon($result, $setup) {
+            return '<img style="height: 100px" class="icon" src="data:image/jpeg;base64,' . base64_encode($result["icon"]) . '" >';
+        }
         function actions($result, $setup)
         {
             $attendantId = $result["id_attendants"];
@@ -42,14 +45,30 @@ require "./adminFunctions.php";
         $resultEventId = $result->eventId;
         $resultSubeventId = $result->subeventId;
 
-        //Get classrooms for subevent
-        $stmt = $conn->prepare("SELECT cs.id_classrooms, c.name FROM classrooms_subevents_teamPropaganda cs JOIN classrooms_teamPropaganda c ON cs.id_classrooms = c.id_classrooms WHERE cs.id_subevents = ?;");
-        if (!$stmt->bind_param("i", $resultSubeventId) || !$stmt->execute() || !$stmt->store_result()) {
+        //Get events
+        $stmt = $conn->prepare("SELECT id_company_days, name FROM company_days_teamPropaganda");
+        if (!$stmt->execute() || !$stmt->store_result()) {
             $stmt->close();
-            echo "<h1>Nelze získat informace o učebnách.</h1>";
+            echo "<h1>Nelze získat informace o dnech firem.</h1>";
             die();
         }
-        echo "<datalist id='classrooms'>";
+        echo "<datalist id='daysList'>";
+        echo "<option label='Žádný' value='NULL'></option>";
+        for ($i = 0; $i < $stmt->num_rows; $i++) {
+            $stmt->bind_result($idEvents, $eventName);
+            $stmt->fetch();
+            echo "<option label='$eventName' value='$idEvents'></option>";
+        }
+        echo "</datalist>";
+
+        //Get fields for subevent
+        $stmt = $conn->prepare("SELECT id_fields, CONCAT(name, ' (',short,')') FROM fields_teamPropaganda;");
+        if (!$stmt->execute() || !$stmt->store_result()) {
+            $stmt->close();
+            echo "<h1>Nelze získat informace o oborech.</h1>";
+            die();
+        }
+        echo "<datalist id='fieldsList'>";
         echo "<option label='Žádná' value='NULL'></option>";
         for ($i = 0; $i < $stmt->num_rows; $i++) {
             $stmt->bind_result($idClassroom, $classroomName);
@@ -64,8 +83,8 @@ require "./adminFunctions.php";
             $conn,
             $result,
             "purkynkaTableStripped purkynkaTableFullLines",
-            "c.id_companies, c.name, c.icon, c.short_info, c.long_info, CONCAT(u.name, ' ', u.surname) AS uFullName, (SELECT GROUP_CONCAT(CONCAT(cf.id_fields, ',',f.name, ' (', f.short,')')) FROM companies_fields_teamPropaganda cf JOIN fields_teamPropaganda f ON cf.id_fields = f.id_fields WHERE cf.id_companies = c.id_companies) AS wanted,GROUP_CONCAT(cdc.id_company_days) visitedDaysIds, GROUP_CONCAT(cd.name) visitedDaysNames",
-            "companies_teamPropaganda c JOIN users_teamPropaganda u ON c.id_users = u.id_users LEFT JOIN company_days_companies_teamPropaganda cdc ON cdc.id_companies = c.id_companies LEFT JOIN company_days_teamPropaganda cd ON cd.id_company_days = cdc.id_company_days",
+            "c.id_companies, c.name as cName, c.icon, c.short_info, c.long_info, CONCAT(u.name, ' ', u.surname) AS uFullName,u.email,GROUP_CONCAT(f.id_fields) as wantedIds, GROUP_CONCAT(CONCAT(f.name, ' (',f.short,')')) as wanted",
+            "companies_teamPropaganda c JOIN users_teamPropaganda u ON c.id_users = u.id_users LEFT JOIN company_days_companies_teamPropaganda cdc ON cdc.id_companies = c.id_companies LEFT JOIN company_days_teamPropaganda cd ON cd.id_company_days = cdc.id_company_days LEFT JOIN companies_fields_teamPropaganda cf ON cf.id_companies = c.id_companies LEFT JOIN fields_teamPropaganda f ON cf.id_fields = f.id_fields",
             "(? IS NULL OR cdc.id_company_days = ?)",
             "c.id_companies;",
             "",
@@ -78,17 +97,18 @@ require "./adminFunctions.php";
                 new filterSelector("email", "Email zástupce firmy", "email", filterSelectorType::TEXT, filterCompareOperator::LIKE),
                 new filterSelector("c.short_info", "Krátký popis", "shortInfo", filterSelectorType::TEXT, filterCompareOperator::LIKE, false),
                 new filterSelector("c.long_info", "Dlouhý popis", "longInfo", filterSelectorType::TEXTAREA, filterCompareOperator::LIKE,false),
-                new filterSelector("wanted", "Zájem o obor", "wanted", filterSelectorType::SELECT, filterCompareOperator::IN,true),
-                new filterSelector("event", "Den firem", "cdc.id_company_days", filterSelectorType::SELECT, filterCompareOperator::IN,true,["listId" => "daysList"]),
+                new filterSelector("wanted", "Zájem o obor", "wanted", filterSelectorType::SELECT, filterCompareOperator::IN,true,["listId" => "fieldsList"]),
+                new filterSelector("cdc.id_company_days", "Den firem", "event", filterSelectorType::SELECT, filterCompareOperator::IN,false,["listId" => "daysList"]),
             ],
             [
                 new filterDisplayer("!actions", "Akce", true),
+                new filterDisplayer("!icon", "Logo společnosti", false),
                 new filterDisplayer("cName", "Název společnosti", true),
                 new filterDisplayer("uFullName", "Zástupce firmy", true),
                 new filterDisplayer("!userEmail", "Email zástupce firmy", true),
                 new filterDisplayer("short_info", "Krátký popis", true),
                 new filterDisplayer("long_info", "Dlouhý popis", false, filterSelectorType::TEXTAREA),
-                new filterDisplayer("wantedNames", "Zájem o obory", true,filterSelectorType::SELECT),
+                new filterDisplayer("wanted", "Zájem o obory", true,filterSelectorType::SELECT),
             ]
         );
         ?>
