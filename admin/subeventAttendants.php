332d93a9d306bfaeff7ac875220b6958bb0cc8b3
LukasJarolim <lukasjarolim01@seznam.cz>
2026-06-17 18:06:17 +0200

    Base PHP

---
+++ admin/subeventAttendants.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require "../assets/config.php";
+require "./adminFunctions.php";
+?>
+
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="form-icons-main-db" content="../formWebScripts/formIcons.json">
+    <meta name="form-icons-db" content="../assets/formIcons.json">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="form-locales-main" content="../formWebScripts/locales/">
+    <title>Rozřazení zájemců</title>
+    <link rel="stylesheet" href="../formWebScripts/css/formStyle.css">
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+
+<body class="pageHolder">
+    <header>
+        <?php $result = setupTitlebarAdmin($conn, "subeventAttendants.php"); ?>
+    </header>
+    <main>
+        <?php  
+        
+        ?>
+    </main>
+</body>
